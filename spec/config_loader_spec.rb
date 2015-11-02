72e53bd38c45b36e9d449dc7d2edfa82ef047e60
futo82 <tony.fu@nih.gov>
2015-11-02 11:03:19 -0500

    Add rspec unit tests for ConfigLoader

--- spec/config_loader_spec.rb
+++ spec/config_loader_spec.rb
@@ -1,9 +1,31 @@
+require 'yaml'
+
 require "#{File.dirname(__FILE__)}/../scripts/PatientRejoinMatchboxScanner/lib/config_loader"
 
 RSpec.describe ConfigLoader, '#initialize' do
   context 'with invalid configuration path' do
-    it 'an exception should be thrown' do
-      # TODO: Implement it.
+    it 'an exception should be raised' do
+      expect { ConfigLoader.new('../config/dummy.yml', 'development') }.to raise_error(Errno::ENOENT)
+    end
+  end
+
+  context 'with a valid configuration path' do
+    it 'parses the configuration file successfully' do
+      cl = ConfigLoader.new('../config/scanner.yml', 'development')
+      parsed_config = cl.config
+
+      expect(parsed_config['database']['hosts']).to eq(['127.0.0.1:27017'])
+      expect(parsed_config['database']['dbname']).to eq('MatchTest')
+
+      expect(parsed_config['match_api']['scheme']).to eq('http')
+      expect(parsed_config['match_api']['hostname']).to eq('127.0.0.1')
+      expect(parsed_config['match_api']['port']).to eq(8080)
+      expect(parsed_config['match_api']['context']).to eq('/match')
+
+      expect(parsed_config['ecog_api']['scheme']).to eq('http')
+      expect(parsed_config['ecog_api']['hostname']).to eq('127.0.0.1')
+      expect(parsed_config['ecog_api']['port']).to eq(3000)
+      expect(parsed_config['ecog_api']['context']).to eq('/MatchInformaticsLayer')
     end
   end
 end

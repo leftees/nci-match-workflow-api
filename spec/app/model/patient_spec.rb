669413cd6e86d6bedfea82c6a4aa6b97e3e9876b
futo82 <tony.fu@nih.gov>
2015-11-24 15:56:52 -0500

    add rspec describe for each patient methods

---
+++ spec/app/model/patient_spec.rb
@@ -0,0 +1,13 @@
+require "#{File.dirname(__FILE__)}/../../../app/model/patient"
+
+RSpec.describe Patient, '#add_prior_drugs' do
+  # Implement unit tests
+end
+
+RSpec.describe Patient, '#add_patient_trigger' do
+  # Implement unit tests
+end
+
+RSpec.describe Patient, '#set_rejoin_date' do
+  # Implement unit tests
+end

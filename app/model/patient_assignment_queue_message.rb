8c01a69c94ee057f67c0f21e49469c547263933f
futo82 <tony.fu@nih.gov>
2015-11-24 14:49:51 -0500

    add accessor attributes to the header and data properties

--- app/model/patient_assignment_queue_message.rb
+++ app/model/patient_assignment_queue_message.rb
@@ -1,4 +1,6 @@
 class PatientAssignmentQueueMessage
+  attr_reader :header, :data
+
   def initialize(patientSequenceNumber)
     @header = { 'type' => 'PATIENT_ASSIGNMENT', 'dateCreated' => DateTime.now }
     @data = { 'patientSequenceNumber' =>  patientSequenceNumber }

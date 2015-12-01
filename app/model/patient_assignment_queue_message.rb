cc64b77dfdc5f65e557f1f43c8896e4a28e2feba
futo82 <tony.fu@nih.gov>
2015-12-01 14:17:50 -0500

    Set the patient assignment queue message dateCreated property to a timestamp instead of a string.

--- app/model/patient_assignment_queue_message.rb
+++ app/model/patient_assignment_queue_message.rb
@@ -2,7 +2,7 @@ class PatientAssignmentQueueMessage
   attr_reader :header, :data
 
   def initialize(patientSequenceNumber)
-    @header = { 'type' => 'PATIENT_ASSIGNMENT', 'dateCreated' => DateTime.now }
+    @header = { 'type' => 'PATIENT_ASSIGNMENT', 'dateCreated' => DateTime.now.strftime('%Q') }
     @data = { 'patientSequenceNumber' =>  patientSequenceNumber }
   end
 end

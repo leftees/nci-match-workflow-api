28089ff37e92688b4bb1539fc49bf01f5d828096
jeremypumphrey <jeremypumphrey@gmail.com>
2016-01-07 14:13:09 -0600

    checking in broken test, trying to get help

--- spec/app/Requests/test_change_request_service_spec.rb
+++ spec/app/Requests/test_change_request_service_spec.rb
@@ -5,17 +5,17 @@ require 'rack/test'
 require 'sinatra'
 
 # describe 'Change Request Service Tests' do
-# RSpec.describe Sinatra::WorkflowApi::ChangeRequestService::ChangeRequest do
-#   def app
-#     Sinatra::WorkflowApi::ChangeRequestService::ChangeRequest
-#     # Sinatra::WorkflowApi::ChangeRequestService
-#   end
+RSpec.describe Sinatra::WorkflowApi::ChangeRequestService::ChangeRequest do
+  def app
+    Sinatra::WorkflowApi::ChangeRequestService::ChangeRequest
+    # Sinatra::WorkflowApi::ChangeRequestService
+  end
 
-  # describe 'Get version info' do
-  #   it 'should pass, returns version' do
-  #     get '/changerequest/info'
-  #     expect(last_response.status).to eq 200
-  #   end
-  # end
+  describe 'Get version info' do
+    it 'should pass, returns version' do
+      get '/changerequest/info'
+      expect(last_response.status).to eq 200
+    end
+  end
 
-# end
+end

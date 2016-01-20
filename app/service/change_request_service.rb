b999c450fb1c10a80af094949bc057ab47b48ddb
jeremypumphrey <jeremypumphrey@gmail.com>
2016-01-20 17:56:43 -0600

    change request cleanup

--- app/service/change_request_service.rb
+++ app/service/change_request_service.rb
@@ -3,20 +3,16 @@ module Sinatra
     module ChangeRequestService
       require 'sinatra/base'
       require "#{File.dirname(__FILE__)}/../util/filesystem_config"
-      # require 'sinatra/config_file'
-      # class ChangeRequest < Sinatra::Base
+
       def self.registered(service)
 
         baseChangeDirectory = FileSystemConfig.local_storage_path + '/change_request_files'
 
         service.post '/changerequest/:patientID' do
-            # upload with:
-            #curl -v -F "data=@/path/to/filename.ext"  http://localhost:9292/changerequest/PatientID
             datafile = params[:data]
             fullpath = "#{baseChangeDirectory}/#{params[:patientID]}/#{datafile[:filename]}"
             WorkflowLogger.logger.info "WORKFLOW API | Change Request, processing upload #{fullpath} ..."
 
-            # check if file already exists
             if File.exist?(fullpath)
               WorkflowLogger.logger.info "WORKFLOW API | Change Request, ERROR File #{fullpath} already exists, 400"
               body TransactionMessage.new('FAILURE', "File #{params[:patientID]}/#{datafile[:filename]} already exists").to_json
@@ -35,23 +31,13 @@ module Sinatra
               Dir.mkdir(dir.join("/")) unless Dir.exist?(dir.join("/"))
             end
 
-            #write temp file to destination
             diskfile = File.new(fullpath, "w")
-            # diskfile.write("Test file")
-            datafile = params[:data]
             diskfile.write(datafile[:tempfile].read)
             diskfile.close
-            WorkflowLogger.logger.info "WORKFLOW API | Change Request, file upload completed successfully"
+            WorkflowLogger.logger.info "WORKFLOW API | Change Request, file #{fullpath} upload completed successfully"
             status 200
             return "The file was successfully uploaded!"
         end
-
-        # service.get '/changerequest/info' do
-        #   content_type :json
-        #   version = Version.instance
-        #   WorkflowLogger.logger.info "WORKFLOW API | Returning version '#{version.to_json}' to remote host."
-        #   version.to_json
-        # end
 
         service.get '/changerequest/:patientID' do
           content_type :json
@@ -68,7 +54,6 @@ module Sinatra
         end
 
         service.get '/changerequest/:patientID/:filename' do
-          # content_type :json
           fullpath = "#{baseChangeDirectory}/#{params[:patientID]}/#{params[:filename]}"
           if File.exist?(fullpath)
             WorkflowLogger.logger.info "WORKFLOW API | Change Request, file for patient returned successfully"
@@ -81,8 +66,6 @@ module Sinatra
         end
 
       end
-      # end
-
     end
   end
 end
